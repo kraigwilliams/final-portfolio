172c48c0eaa71a5caa6e924f50682440a9e91fbf
KRAIGYG <williamskrg@gmail.com>
2020-04-26 18:42:52 -0400

    test about me

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ class Home extends Component{
             <h1>Hi, I'm Kraig</h1>
             <p>I'm a FullStack Javascript Developer</p>
             <p>Welcome to my Porfolio. Here you will find my <Link to= '/projects'>best projects</Link> and an <Link to='/about-me'>about me</Link> section where you can get to know me better.</p>
-            <p> If you would like to contact me about becoming a part of your talented development team I love to speak with you. You may contact me directly through any of the links below. </p>
+            <p> If you would like to contact me about becoming a part of your talented development team I would love to speak with you. You may contact me directly through any of the links below. </p>
             </div>
             </section>
         )
